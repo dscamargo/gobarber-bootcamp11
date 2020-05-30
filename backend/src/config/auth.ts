1886a7411a090dcc75ad4f72ace72c1106002119
Douglas S. Camargo <douglas_simon@hotmail.com>
2020-05-29 22:35:54 -0300

    feat: dotenv added on app

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'e12c002c44f9ec36a6b71936211e44a2',
-    expiresIn: '1d',
+    secret: process.env.APP_SECRET,
+    expiresIn: process.env.EXPIRES_IN,
   },
 };
